52576b64cca2ba04c31c9e5fdfeae94b71493aec
miiin0324 <miiin0324@gmail.com>
2022-07-01 15:21:52 +0900

    Add vanilla todos 초기 세팅

---
+++ js/todo.js
@@ -0,0 +1,17 @@
+let todoList = [
+  {
+    id: 1,
+    text: "블로그 정리",
+  },
+];
+
+// TODO 초기화 함수
+const init = () => {};
+
+// TODO 값 추가 함수
+const addTodo = () => {};
+
+// TODO 다시 화면에 그려주는 함수
+const render = () => {};
+
+init();
