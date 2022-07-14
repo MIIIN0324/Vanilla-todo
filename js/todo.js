bbb980f6f8cb23ca876c055a556addf9088d99cd
miiin0324 <miiin0324@gmail.com>
2022-07-14 13:19:29 +0900

    Modify - html, js

--- js/todo.js
+++ js/todo.js
@@ -1,107 +1,30 @@
-// let todoList = [
-//   {
-//     id: 1,
-//     text: "블로그 정리",
-//   },
-// ];
+const todoForm = document.querySelector("#todoForm");
+const todoInput = document.querySelector("input");
+const todoList = document.querySelector("#todoList");
 
-// // TODO 초기화 함수
-// const init = () => {};
+let todos = [];
 
-// // TODO 값 추가 함수
-// const addTodo = () => {};
+//추가 함수
+const handleTodoSubmit = (event) => {
+    event.preventDefault();
+    if (todoInput.value.length >= 1) {
+        const item = {
+            id: Date.now(),
+            text: todoInput.value,
+        };
+        todos.push(item);
+        renderTodo(item);
+        todoInput.value = "";
+    }
+};
 
-// // TODO 다시 화면에 그려주는 함수
-// const render = () => {};
-
-// init();
-
-const todoInput = document.querySelector(".addInput");
-const todos = document.querySelector(".todos");
-
-const paintTodo = (todo) => {
+//화면에 그려주는 함수
+const renderTodo = (item) => {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    span.innerHTML = todo;
+    span.innerText = item.text;
     li.appendChild(span);
-    todos.appendChild(li);
+    todoList.appendChild(li);
 };
 
-const createTodo = (event) => {
-    event.preventDefault();
-    const todo = todoInput.value;
-    paintTodo(todo);
-    todoInput.value = "";
-};
-
-const init = () => {};
-init();
-
-// const todoInputElem = document.querySelector(".todoInput");
-// const todoListElem = document.querySelector(".todoList");
-
-// let todos = [];
-// let id = [];
-
-// const setTodos = (newTodos) => {
-//     todos = newTodos;
-// };
-
-// const getAllTodos = () => {
-//     return todos;
-// };
-
-// const appendTodos = (text) => {
-//     const newId = id++;
-//     const newTodos = getAllTodos().concat({
-//         id: newId,
-//         isCompleted: false,
-//         content: text,
-//     });
-//     setTodos(newTodos);
-//     paintTodos();
-// };
-
-// //할 일 추가될 때마다, paintTodos()함수 실행하여 렌더링
-// const paintTodos = () => {
-//     todoListElem.innerHTML = null; //todoListElem 요소 안의 HTML 초기화
-//     const allTodos = getAllTodos(); //todos 배열 가져오기
-
-//     // "todo-item"에 해당하는 HTML을 그려서 "todo-list"에 추가하기
-//     allTodos.forEach((todo) => {
-//         const todoItemElem = document.createElement("li");
-//         todoItemElem.classList.add("todoItem");
-
-//         const checkboxElem = document.createElement("div");
-//         checkboxElem.classList.add("checkBox");
-
-//         const todoElem = document.createElement("div");
-//         todoElem.classList.add("todo");
-//         todoElem.innerText = todo.content;
-
-//         const delBtnElem = document.createElement("button");
-//         delBtnElem.classList.add("delBtn");
-//         delBtnElem.innerHTML = "x";
-
-//         if (todo.isChecked) {
-//             todoItemElem.classList.add("checked");
-//             checkboxElem.innerText = "✔";
-//         }
-//         todoItemElem.appendChild(checkboxElem);
-//         todoItemElem.appendChild(todoElem);
-//         todoItemElem.appendChild(delBtnElem);
-
-//         todoListElem.appendChild(todoItemElem);
-//     });
-// };
-
-// const init = () => {
-//     todoInputElem.addEventListener("keypress", (e) => {
-//         if (e.key === "Enter") {
-//             appendTodos(e.target.value);
-//             todoInputElem.value = "";
-//         }
-//     });
-// };
-
-// init();
+todoForm.addEventListener("click", handleTodoSubmit);
